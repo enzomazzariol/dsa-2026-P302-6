793f549ea45f112659b5b67d31ccefa1cb876a71
ttoniturtorres <ttoniturtorres@gmail.com>
2026-04-18 13:16:07 +0200

    Fix: Forzar formato de linea Unix y ejecucion

--- check-format.sh
+++ check-format.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-output=$(clang-format --dry-run -Werror **/*.c **/*.h 2>&1)
-
-if [[ -n "$output" ]]; then
-  echo "$output"
-  echo "To fix it, run: make f"
-  exit 1
-else
-  exit 0
-fi 
+echo 'Iniciando comprobacion...'
+make cf
