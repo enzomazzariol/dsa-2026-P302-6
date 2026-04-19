c7e61df7aeb26e6150f361f5273ed30eeb28a5ec
ttoniturtorres <toni.tur01@estudiant.upf.edu>
2026-04-19 17:06:18 +0200

    prueba

--- src/main.c
+++ src/main.c
@@ -18,6 +18,7 @@ int main(void) {
   printf("*****************\nWelcome to DSA!\n*****************\n");
 
   // how to import and call a function
+  
   printf("Factorial of 4 is %d\n", fact(4));
 
   // uncomment and run "make v" to see how valgrind detects memory leaks
